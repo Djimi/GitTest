e1482ad3482f1f23da2b060dbae48998b8511503
Djimi <damyanmanev@gmail.com>
2018-05-13 16:56:45 +0300

    Manufacturer property and accessors and mutators for it added in the Car class

--- git-test/src/main/java/com/git/git_test/models/Car.java
+++ git-test/src/main/java/com/git/git_test/models/Car.java
@@ -1,5 +1,15 @@
 package com.git.git_test.models;
 
 public class Car {
+	private String manufacturer;
 
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	
+	
 }
