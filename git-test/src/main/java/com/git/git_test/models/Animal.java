9166b32e7eac55897adc789103552c5258564fdd
DjimiGuest <damyanmanev@gmail.com>
2018-05-27 20:49:36 +0300

    type property of Animal class added

--- git-test/src/main/java/com/git/git_test/models/Animal.java
+++ git-test/src/main/java/com/git/git_test/models/Animal.java
@@ -1,5 +1,14 @@
 package com.git.git_test.models;
 
 public class Animal {
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
 }
