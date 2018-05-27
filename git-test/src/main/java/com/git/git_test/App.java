c06352734fb023db041741909a72cba6b3e3e3e6
Djim <damyanmanev@gmail.com>
2018-05-27 20:22:40 +0300

    App class added final

--- git-test/src/main/java/com/git/git_test/App.java
+++ git-test/src/main/java/com/git/git_test/App.java
@@ -4,10 +4,8 @@ package com.git.git_test;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
