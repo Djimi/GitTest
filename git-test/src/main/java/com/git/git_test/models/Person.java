d24c9e927fec89e37dc99e80a5b79499260cbba1
Djim <damyanmanev@gmail.com>
2018-09-06 14:05:00 +0300

    Name field added to PErson class

--- git-test/src/main/java/com/git/git_test/models/Person.java
+++ git-test/src/main/java/com/git/git_test/models/Person.java
@@ -1,5 +1,13 @@
 package com.git.git_test.models;
 
 public class Person {
+    private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
