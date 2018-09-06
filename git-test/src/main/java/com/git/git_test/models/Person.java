c3c6c54bbd5dfdc4af354b87f16d6b736a95f239
Djim <damyanmanev@gmail.com>
2018-09-06 14:06:55 +0300

    Age field added to Person class

--- git-test/src/main/java/com/git/git_test/models/Person.java
+++ git-test/src/main/java/com/git/git_test/models/Person.java
@@ -2,6 +2,7 @@ package com.git.git_test.models;
 
 public class Person {
     private String name;
+    private int age;
 
     public String getName() {
         return name;
@@ -9,5 +10,19 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
     }
 }
